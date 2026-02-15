173357bd524f6df9830ebc0287420e1d2f53a8ea
Art Vincent D. Papolonias <artvincent.papolonias@g.msuiit.edu.ph>
2026-02-15 21:11:24 +0800

    Data Cruncher!!

---
+++ DataCruncher.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DataCruncher {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        
+        int [] temps = new int[5];
+
+        int max = temps[0];
+        for (int i = 0; i < 5; i++){
+            System.out.println("Enter the Temperature the last 5 days");
+            System.err.println("Day " + (i+1) + ":°C ");
+            temps[i] = scn.nextInt();
+        }
+
+        for (int i=0; i < 5; i++){
+            if (temps[i]>=max){
+                max = temps[i];
+            }
+        }
+        double sum = 0;
+
+        for(int i=0; i < temps.length; i++){
+            sum = sum + temps[i];
+        }
+
+        double average = sum / temps.length;
+        System.out.println("The Average temperature in the Last 5 days: °C " + average);
+        System.out.println("The Highest Temperature in the Last 5 Days: °C " + max);
+    }
+}
