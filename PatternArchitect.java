b60aeb213280d51fd04562e0cc1748c4777e2731
Art Vincent D. Papolonias <artvincent.papolonias@g.msuiit.edu.ph>
2026-02-15 22:06:03 +0800

    LAST!

--- PatternArchitect.java
+++ PatternArchitect.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
 public class PatternArchitect {
-    
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        System.out.println("Give a Number(Max 10): ");
+        int number = scn.nextInt();
+
+        for (int i = 1; i<=number; i++){
+            for (int j=1; j <= i; j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+
+
+
+        for (int i = number; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print("*");
+            }System.out.println();
+        }
+    }
 }
+
+// still need to recall.
